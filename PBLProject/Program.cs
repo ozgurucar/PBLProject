4af8a282d0cf79a35580def75731a869a7e735f3
Özgür Uçar <ucar.ozgur@outlook.com>
2024-10-12 20:51:59 +0300

    ai method is done but need to be checked

--- PBLProject/Program.cs
+++ PBLProject/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 Random rnd = new Random();
 
-int gameMode;
+int gameMode = 2;
 int turn = 1;
 
 String a1 = initializeBoard();
@@ -19,34 +19,25 @@ String a8 = initializeBoard();
 String a9 = initializeBoard();
 String allStrings = a1 + a2 + a3 + a4 + a5 + a6 + a7 + a8 + a9;
 
-String row1 = a1 + a2 + a3;
-String row2 = a4 + a5 + a6;
-String row3 = a7 + a8 + a9;
-
-String col1 = a1 + a4 + a7;
-String col2 = a2 + a5 + a8;
-String col3 = a3 + a6 + a9;
-
-String diagonal1 = a1 + a5 + a9;
-String diagonal2 = a3 + a5 + a7;
-
-int initialPoints = calculateAllBoard();
+int initialPoints = calculateAllBoard(a1,a2,a3,a4,a5,a6,a7,a8,a9);
 int pointsOfHuman = initialPoints;
 int pointsOfComputer = initialPoints;
 
-int calculateAllBoard()
+AIPlay();
+
+int calculateAllBoard(String l1, String l2, String l3, String l4, String l5, String l6, String l7, String l8, String l9)
 {
     int points = 0;
-    points += calculatePoints(row1);
-    points += calculatePoints(row2);
-    points += calculatePoints(row3);
+    points += calculatePoints(l1+l2+l3);
+    points += calculatePoints(l4+l5+l6);
+    points += calculatePoints(l7+l8+l9);
 
-    points += calculatePoints(col1);
-    points += calculatePoints(col2);
-    points += calculatePoints(col3);
+    points += calculatePoints(l1+l4+l7);
+    points += calculatePoints(l2+l5+l8);
+    points += calculatePoints(l3+l6+l9);
 
-    points += calculatePoints(diagonal1);
-    points += calculatePoints(diagonal2);
+    points += calculatePoints(l1+l5+l9);
+    points += calculatePoints(l3+l5+l7);
 
     return points;
 }
@@ -161,8 +152,16 @@ Console.WriteLine(pointsOfComputer);
 
 void AIPlay()
 {
-    int max = -1;
     int tryCount = 0;
+
+    
+
+    int max = calculateAllBoard(a1, a2, a3, a4, a5, a6, a7, a8, a9);
+    String maxString = "";
+    int maxLocation = -1;
+
+
+    
     switch (gameMode)
     {
         case 0:
@@ -173,37 +172,130 @@ void AIPlay()
             tryCount = 100; break;
     }
 
-    for(int i = 0; i < tryCount; i++)
+
+    for (int i = 0; i < tryCount; i++)
     {
-        String allStrings = a1 + a2 + a3+ a4 + a5+ a6 + a7 + a8 + a9;
-        int randomPoint = rnd.Next(1, 10);
 
-        int tempPoint = -1;
-        String tempLetter = "";
+        String tryStr = initializeBoard();
 
-        String randomLetter = initializeBoard();
-        switch(randomPoint)
+        String b1 = a1;
+        String b2 = a2;
+        String b3 = a3;
+        String b4 = a4;
+        String b5 = a5;
+        String b6 = a6;
+        String b7 = a7;
+        String b8 = a8;
+        String b9 = a9;
+        int location = rnd.Next(1, 10);
+        if (location == 1)
         {
-            case 1: 
-                a1 = randomLetter; break;
-            case 2:
-                a2 = randomLetter; break;
-            case 3:
-                a3 = randomLetter; break;
-            case 4:
-                a4 = randomLetter; break;
-            case 5:
-                a5 = randomLetter; break;
-            case 6:
-                a6 = randomLetter; break;
-            case 7:
-                a7 = randomLetter; break;
-            case 8:
-                a8 = randomLetter; break;
-            case 9:
-                a9 = randomLetter; break;
-        }
-        max = calculateAllBoard();
-    }
+            b1 = tryStr;
+            if (calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9) > max)
+            {
+                max = calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9);
+                maxString = tryStr;
+                maxLocation = location;
 
+            }
+        }
+
+        else if (location == 2)
+        {
+            b2 = tryStr;
+            if (calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9) > max)
+            {
+                max = calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9);
+                maxString = tryStr;
+                maxLocation = location;
+
+            }
+        }
+
+        else if (location == 3)
+        {
+            b3 = tryStr;
+            if (calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9) > max)
+            {
+                max = calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9);
+                maxString = tryStr;
+                maxLocation = location;
+
+            }
+        }
+
+        else if (location == 4)
+        {
+            b4 = tryStr;
+            if (calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9) > max)
+            {
+                max = calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9);
+                maxString = tryStr;
+                maxLocation = location;
+
+            }
+        }
+
+        else if (location == 5)
+        {
+            b5 = tryStr;
+            if (calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9) > max)
+            {
+                max = calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9);
+                maxString = tryStr;
+                maxLocation = location;
+
+            }
+        }
+
+        else if (location == 6)
+        {
+            b6 = tryStr;
+            if (calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9) > max)
+            {
+                max = calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9);
+                maxString = tryStr;
+                maxLocation = location;
+
+            }
+        }
+
+        else if (location == 7)
+        {
+            b7 = tryStr;
+            if (calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9) > max)
+            {
+                max = calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9);
+                maxString = tryStr;
+                maxLocation = location;
+
+            }
+        }
+
+        else if (location == 8)
+        {
+            b8 = tryStr;
+            if (calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9) > max)
+            {
+                max = calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9);
+                maxString = tryStr;
+                maxLocation = location;
+
+            }
+        }
+
+        else if (location == 9)
+        {
+            b9 = tryStr;
+            if (calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9) > max)
+            {
+                max = calculateAllBoard(b1, b2, b3, b4, b5, b6, b7, b8, b9);
+                maxString = tryStr;
+                maxLocation = location;
+
+            }
+        }
+        Console.WriteLine(max + maxString + " " + maxLocation);
+
+    }
     }
